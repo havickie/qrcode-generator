d8193f5167bca43bf20d4b910e16575f9d28eb0c
victor <victorfrancis612@gmail.com>
2022-10-26 05:06:12 +0100

    first commit

---
+++ wifi.js
@@ -0,0 +1,27 @@
+
+
+
+
+
+
+
+
+const hide = (el) => el.style.setProperty('display', 'none');
+const show = (el) => el.style.setProperty('display', 'block');
+
+// wifi
+function generateQrCode() {
+  new QRious({
+    element: document.getElementById('qrcode'),
+    value: getQrCodeValue(),
+    size: 200,
+  });
+}
+
+function getQrCodeValue() {
+  const ssid = document.getElementById('ssid').value;
+  const password = document.getElementById('password').value;
+  const type = document.querySelector('input[name="type"]:checked').value;
+  const hidden = document.getElementById('hidden').checked;
+  return `WIFI:S:${ssid};T:${type};P:${password};H:${hidden};;`;
+}
